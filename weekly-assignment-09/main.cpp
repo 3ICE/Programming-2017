46fd0bddd0c918096061e6aa19842c0c3a8d0171
Daniel Berezvai <daniel.berezvai@student.tut.fi>
2017-03-23 21:40:02 +0200

    Weekly Exercise 9 ready and tested

---
+++ weekly-assignment-09/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "There are no actual exercise problems for this week, not even weekly assignment. "
+	<< "Instead the exercise periods are used as tutoring sessions for the project 01. "
+	<< "The TA will be available to answer your questions regarding any problems you might have faced when working on you project." << endl
+	<< "The exercise periods will be in kooditorio TC215 (as always but this week it is official)." << endl;
+}
